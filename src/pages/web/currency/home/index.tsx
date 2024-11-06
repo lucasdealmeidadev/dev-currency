eaf7adcf6c0351a35bd58b723e7086c47f83db66
Lucas Almeida <lucasdealmeida.monteiro@gmail.com>
2024-11-06 10:51:55 -0300

    Criando página home pt2

--- src/pages/web/currency/home/index.tsx
+++ src/pages/web/currency/home/index.tsx
@@ -1,9 +1,52 @@
 import styles from './home.module.css'
+import { BsSearch } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
-    <div>
-      <h1>Página Home</h1>
-    </div>
+    <main className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="Digite o nome da moeda... Ex: beeb" />
+
+        <button type="submit">
+          <BsSearch size={30} color="#fff" />
+        </button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor Mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <div className={styles.name}>
+                <Link to={'/detail/bitcoin'}>
+                  <span>Bitcoin</span> | BTC
+                </Link>
+              </div>
+            </td>
+            <td className={styles.tdLabel} data-label="Valor Mercado">
+              1T
+            </td>
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+            <td className={styles.tdLabel} data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
   )
 }
